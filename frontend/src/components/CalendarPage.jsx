b82d7169714b400503df0a626fbd090e8ab77c4a
Kass 🌸 <kassandra.ferland@gmail.com>
2024-07-26 16:46:56 -0400

    calendar works and shows dots

--- frontend/src/components/CalendarPage.jsx
+++ frontend/src/components/CalendarPage.jsx
@@ -1,11 +1,69 @@
-// import Header from "./Header"
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Calendar from 'react-calendar'
+import axios from 'axios'
+import Header from './Header'
+import 'react-calendar/dist/Calendar.css'
 
-export default function CalendarPage() {
+const CalendarPage = () => {
+  const { username } = useParams()
+  const [datesWithStatus, setDatesWithStatus] = useState({})
 
-    return (
-        <div>
-    {/* <Header /> */}
-    <h1>calendar</h1>
+  useEffect(() => {
+    const getDayData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/days/${username}/`)
+        const data = response.data
+        const datesStatus = data.reduce((acc, day) => {
+          acc[day.date] = {
+            good_day: day.good_day,
+            neutral_day: day.neutral_day,
+            nauseous: day.nauseous,
+            fainting: day.fainting,
+            bed_bound: day.bed_bound
+          }
+          return acc
+        }, {})
+        setDatesWithStatus(datesStatus)
+      } catch (error) {
+        console.error('Error getting data:', error)
+      }
+    }
+    getDayData()
+  }, [username])
+
+//   •	data is the array being reduced.
+//   •	acc is the accumulator, which starts as an empty object ({}) and is used to build up the final result.
+//   •	day is the current item in the array as reduce iterates over it.
+
+
+  const tileContent = ({ date, view }) => {
+    if (view === 'month') {
+      const dateString = date.toISOString().split('T')[0]
+      const status = datesWithStatus[dateString]
+      if (status) {
+        const dots = []
+        if (status.good_day) dots.push(<div key="good" className="dot good-day"></div>)
+        if (status.neutral_day) dots.push(<div key="neutral" className="dot neutral-day"></div>)
+        if (status.nauseous) dots.push(<div key="nauseous" className="dot nauseous"></div>)
+        if (status.fainting) dots.push(<div key="fainting" className="dot fainting"></div>)
+        if (status.bed_bound) dots.push(<div key="bed_bound" className="dot bed-bound"></div>)
+        return <div className="dots">{dots}</div>
+      }
+    }
+    return null
+  }
+
+  return (
+    <div className="calendar-page">
+    <Header/>
+        
+      <Calendar
+        tileContent={tileContent}
+        // calendarType="ISO 8601"
+      />
     </div>
-)
+  )
 }
+
+export default CalendarPage
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -61,7 +61,7 @@ export default function Header({ onDateChange }) {
 
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/login"><img src='https://i.imgur.com/QpaCVKn.png' width={40} alt="Logo" /></Navbar.Brand>
+      {/* <Navbar.Brand href="/login"><img src='https://i.imgur.com/QpaCVKn.png' width={40} alt="Logo" /></Navbar.Brand> */}
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
@@ -70,9 +70,17 @@ export default function Header({ onDateChange }) {
               <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
               {(isCalendarPage || isHomePage) && (
                 <Nav className="calendar">
-                  <button onClick={handlePreviousDay}>{'<'}</button>
-                  <span>{format(currentDate, 'MM-dd')}</span>
-                  <button onClick={handleNextDay} disabled={isAfter(currentDate, new Date())}>{'>'}</button>
+                 <button height="10" onClick={handlePreviousDay}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-heart-arrow flipped-arrow" style={{ transform: 'scaleX(-1)' }} viewBox="0 0 16 16">
+                    <path d="M6.707 9h4.364c-.536 1.573 2.028 3.806 4.929-.5-2.9-4.306-5.465-2.073-4.929-.5H6.707L4.854 6.146a.5.5 0 1 0-.708.708L5.293 8h-.586L2.854 6.146a.5.5 0 1 0-.708.708L3.293 8h-.586L.854 6.146a.5.5 0 1 0-.708.708L1.793 8.5.146 10.146a.5.5 0 0 0 .708.708L2.707 9h.586l-1.147 1.146a.5.5 0 0 0 .708.708L4.707 9h.586l-1.147 1.146a.5.5 0 0 0 .708.708z"/>
+                  </svg>
+                </button>
+                  <span>{format(currentDate, 'M.dd')}</span>
+                  <button onClick={handleNextDay} disabled={isAfter(currentDate, new Date())}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-heart-arrow" viewBox="0 0 16 16">
+                    <path d="M6.707 9h4.364c-.536 1.573 2.028 3.806 4.929-.5-2.9-4.306-5.465-2.073-4.929-.5H6.707L4.854 6.146a.5.5 0 1 0-.708.708L5.293 8h-.586L2.854 6.146a.5.5 0 1 0-.708.708L3.293 8h-.586L.854 6.146a.5.5 0 1 0-.708.708L1.793 8.5.146 10.146a.5.5 0 0 0 .708.708L2.707 9h.586l-1.147 1.146a.5.5 0 0 0 .708.708L4.707 9h.586l-1.147 1.146a.5.5 0 0 0 .708.708z"/>
+                  </svg>
+                </button>
                 </Nav>
               )}
             </>
@@ -86,17 +94,17 @@ export default function Header({ onDateChange }) {
           <Nav className='ml-auto'>
             {!isCalendarPage && (
               <Link to={`/calendar/${username}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-heart-fill" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="auto" fill="currentColor" className="bi bi-calendar-heart-fill" viewBox="0 0 16 16">
                   <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2M8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
                 </svg>
               </Link>
             )}
             {isCalendarPage && (
               <Link to={`/home/${username}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
-                  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1V.5a.5.5 0 0 0-.5.5v1.293z"/>
-                  <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house-heart-fill" viewBox="0 0 16 16">
+                <path d="M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.707L8 2.207 1.354 8.853a.5.5 0 1 1-.708-.707z"/>
+                <path d="m14 9.293-6-6-6 6V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5zm-6-.811c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.691 0-5.018"/>
+              </svg>
               </Link>
             )}
           </Nav>
